5f9339246dc2b0555da368235f6675e76e8d5be4
dani04GitHub <dani.grimaldi2004@gmail.com>
2022-12-07 11:30:58 +0100

    iniziailizzo server

---
+++ Server/main.py
@@ -0,0 +1,17 @@
+import socket
+print ('avvio server')
+HOST = '127.0.0.1' 
+PORT = 60001
+s = socket.socket() #senza parametri protocollo tcp-ip
+s.bind((HOST, PORT))
+s.listen()
+conn, addr = s.accept()
+print('Connesso ad IP: ', addr)
+while True:
+    dati = conn.recv(1024)
+    if not dati:
+        break
+    conn.sendall(b'restituisco dati ricevuti:'+dati)
+    print('dati ricevuti: ',dati)
+s.close()    
+print('Fine programma')
