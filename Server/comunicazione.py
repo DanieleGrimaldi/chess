ac0dbbf8f397611e0150a0fbe03b59d0461cf163
dani04GitHub <dani.grimaldi2004@gmail.com>
2022-12-07 16:12:53 +0100

    testo client server

--- Server/comunicazione.py
+++ Server/comunicazione.py
@@ -3,6 +3,6 @@ import threading
 import time
 
 def thread_comunicazione(conn):
-    dati = conn.recv(1024)#ricevo
-    conn.sendall(b'restituisco dati ricevuti:'+dati)#trasmetto
-    
+    dati = conn.recv(1500)#ricevo
+    #conn.sendall(b'restituisco dati ricevuti:'+dati)#trasmetto
+    print(dati)
--- Server/main.py
+++ Server/main.py
@@ -1,20 +1,19 @@
 import socket
 import comunicazione
-
+import threading
 
 print ('avvio server')
 HOST = '127.0.0.1' 
 PORT = 8080
 s = socket.socket() #senza parametri protocollo tcp-ip
 s.bind((HOST, PORT))
-flag=true
+flag=True
 
 while flag:
     s.listen()
     conn, addr = s.accept()
-    with conn:
-        x = threading.Thread(target=comunicazione.thread_comunicazione, args=(conn,))
-        x.start()
+    x = threading.Thread(target=comunicazione.thread_comunicazione, args=(conn,))
+    x.start()
         
 s.close()    
 print('Fine programma')
