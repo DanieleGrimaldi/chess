20ee62b1157ff1a387d8b762126d34bc58fa3460
dani04GitHub <dani.grimaldi2004@gmail.com>
2022-12-14 12:07:38 +0100

    gestione urtenti
    inizio a lavorare sulla comunicazione e la gestione degli tenti

---
+++ Server/partita.py
@@ -0,0 +1,14 @@
+class game:
+    giocatore1
+    giocatore2
+    turno=1
+    
+    def __init__(self,giocatore1,giocatore2):
+        self.giocatore1 = giocatore1
+        self.giocatore2 = giocatore2
+        
+    def mossa(turno,partenza,arrivo):
+        valida=True
+        return valida
+    
+    
